651489284c6de31c5c8b33777a67ed96ad3d2b99
Kronka <33985574+Kronka@users.noreply.github.com>
2021-07-03 14:28:36 +0300

    Add files via upload

---
+++ miniapp/miniapp.NativeActivity/pch.h
@@ -0,0 +1,23 @@
+﻿//
+// pch.h
+// Заголовок стандартных системных включаемых файлов.
+//
+// Используется системой сборки для создания предкомпилированного заголовка. Обратите внимание, что
+// pch.cpp не нужен, а pch.h автоматически включается в состав всех файлов cpp,
+// которые являются частью проекта
+//
+
+#include <jni.h>
+#include <errno.h>
+
+#include <string.h>
+#include <unistd.h>
+#include <sys/resource.h>
+
+#include <EGL/egl.h>
+#include <GLES/gl.h>
+
+#include <android/sensor.h>
+
+#include <android/log.h>
+#include "vendor/android_native_app_glue.h"
